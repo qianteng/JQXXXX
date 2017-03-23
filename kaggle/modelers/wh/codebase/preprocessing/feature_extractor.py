fc0d29c9184fbeb3f1f7a637800c01b892c9891a
Wei Hong <physhunter@gmail.com>
2017-03-23 00:34:11 -0400

    initialze codebase and first attempt

---
+++ kaggle/modelers/wh/codebase/preprocessing/feature_extractor.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from scipy import stats
+
+
+def aggreate_features(df):
+    # group by visit number
+    groupby_vn = df.groupby('VisitNumber', as_index=True)
+    # sum of scancount per visit
+    df = df.join(groupby_vn['ScanCount'].sum(), on='VisitNumber', rsuffix='_sum_groupby_vn')
+    # number of distinct department covered per visit
+    df = df.join(groupby_vn.agg({'Encoded_DepartmentDescription': pd.Series.nunique}), on='VisitNumber',
+                 rsuffix='_nunique_groupby_vn')
+    # number of distinct fineline number covered per visit
+    df = df.join(groupby_vn.agg({'FinelineNumber': pd.Series.nunique}), on='VisitNumber', rsuffix='_nunique_groupby_vn')
+    # number of distinct UPC covered per visit
+    df = df.join(groupby_vn.agg({'Encoded_Upc' : pd.Series.nunique}), on='VisitNumber', rsuffix='_nunique_groupby_vn')
+    # number of items purchased under a single fineline number per visit
+    df = df.join(groupby_vn[['FinelineNumber']].agg(lambda x: stats.mode(x['FinelineNumber']).count[0]),
+                 on='VisitNumber', rsuffix='_max_by_vn')
+    # the fineline number under which the most items are purchased per visit
+    df = df.join(groupby_vn[['FinelineNumber']].agg(lambda x: stats.mode(x['FinelineNumber'])[0]), on='VisitNumber',
+                 rsuffix='_has_max_by_vn')
+    # number of items purchased under a single department per visit
+    df = df.join(groupby_vn[['Encoded_DepartmentDescription']]
+                 .agg(lambda x: stats.mode(x['Encoded_DepartmentDescription']).count[0]), on='VisitNumber',
+                 rsuffix='_max_by_vn')
+    # the department under which the most items are purchased per visit
+    df = df.join(groupby_vn[['Encoded_DepartmentDescription']]
+                 .agg(lambda x: stats.mode(x['Encoded_DepartmentDescription'])[0]), on='VisitNumber',
+                 rsuffix='_has_max_by_vn')
+
+    return df
+
