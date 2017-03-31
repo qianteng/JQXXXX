faf33ef4b289a38029f350a41cceab257517eb6e
Wei Hong <physhunter@gmail.com>
2017-03-31 17:26:06 -0400

    add dummy variables for department description

--- kaggle/modelers/wh/codebase/preprocessing/feature_extractor.py
+++ kaggle/modelers/wh/codebase/preprocessing/feature_extractor.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from scipy import stats
+import numpy as np
 
 
-def aggreate_features(df):
+def aggregate_features(df):
     # group by visit number
     groupby_vn = df.groupby('VisitNumber', as_index=True)
     # sum of scancount per visit
@@ -38,3 +39,31 @@ def aggreate_features(df):
                  rsuffix='_purchased_items_by_vn')
     return df
 
+
+def aggregate_features_v2(df):
+    # group by visit number
+
+    dummies = pd.get_dummies(df.DepartmentDescription)
+    df[dummies.columns] = dummies
+    df[dummies.columns] = df[dummies.columns].apply(lambda x: x * df['ScanCount'])
+
+    groupby_vn = df.groupby('VisitNumber', as_index=True)
+
+    out = pd.DataFrame(index=df['VisitNumber'].unique())
+    if 'TripType' in df.columns:
+        out['TripType'] = groupby_vn['TripType'].max()
+    out['ScanCountSum'] = groupby_vn['ScanCount'].sum()
+    out['Encoded_UPC_nunique'] = groupby_vn.agg({'Encoded_Upc': pd.Series.nunique})
+    out['FinelineNumber_max_num'] = groupby_vn[['FinelineNumber']].agg(lambda x: stats.mode(x['FinelineNumber']).count[0])
+    out['FinelineNumber_has_max'] = groupby_vn[['FinelineNumber']].agg(lambda x: stats.mode(x['FinelineNumber'])[0])
+    #out['Encoded_DepartmentDescription_max_num'] = groupby_vn[['Encoded_DepartmentDescription']].agg(
+        #lambda x: stats.mode(x['Encoded_DepartmentDescription']).count[0])
+    #out['Encoded_DepartmentDescription_has_max'] = groupby_vn[['Encoded_DepartmentDescription']].agg(
+        #lambda x: stats.mode(x['Encoded_DepartmentDescription'])[0])
+    out['Returned_num'] = groupby_vn[['ScanCount']].agg(lambda x: -x[x < 0].sum())
+    out['Purchased_num'] = groupby_vn[['ScanCount']].agg(lambda x: x[x > 0].sum())
+
+    for col_name in dummies.columns:
+        out[col_name] = groupby_vn[[col_name]].agg(np.sum)
+
+    return out
