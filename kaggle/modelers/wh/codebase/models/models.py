fc4526253569d017447ab017d7e3770093e3ffad
Wei Hong <physhunter@gmail.com>
2017-04-10 00:39:50 -0400

    refactor code

--- kaggle/modelers/wh/codebase/models/models.py
+++ kaggle/modelers/wh/codebase/models/models.py
@@ -7,10 +7,8 @@ import pandas as pd
 
 
 class RandomForestMultiClass(BaseEstimator):
-    def __init__(self, n_estimators=None, n_jobs=None):
-        self.n_estimators = 50 if n_estimators is None else n_estimators
-        self.n_jobs = 4 if n_jobs is None else n_jobs
-        self.clf = RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=self.n_jobs)
+    def __init__(self, n_estimators=50, n_jobs=4):
+        self.clf = RandomForestClassifier(n_estimators=n_estimators, n_jobs=n_jobs)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
