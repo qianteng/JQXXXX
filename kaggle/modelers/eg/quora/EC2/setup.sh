4c86de25f2d43d11d5055eb1fe198b759fa22949
Eric Guo <euclidean@172-30-48-214.DYNAPOOL.NYU.EDU>
2017-04-22 12:05:52 -0400

    auto-download test data

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -1,2 +1,2 @@
 wget --directory-prefix=../Data https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv
-wget --directory-prefix=../Data https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv
+wget --directory-prefix=../Data https://www.dropbox.com/s/ze8odkdw1444e92/test.csv
