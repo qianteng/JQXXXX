a81b3d001240e45e511ef5a74f4be2c314fd009c
Eric Guo <euclidean@172-30-48-201.DYNAPOOL.NYU.EDU>
2017-04-22 14:24:04 -0400

    correct setup logic

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -1,11 +1,9 @@
-git clone https://github.com/tomasr/molokai.git
-rm -rf $HOME/.vim
-mv molokai $HOME/.vim
-rm -rf molokai
-
-cp -r ./home/* $HOME
-
 ROOT_DIR="$(git rev-parse --show-toplevel)"/kaggle/modelers/eg/quora
+
+for f in .bash_profile .gitconfig .vim .vimrc git-completion.bash git-prompt.sh
+do
+	cp $ROOT_DIR/EC2/$f $HOME
+done
 
 wget https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
 wget https://www.dropbox.com/s/ze8odkdw1444e92/test.csv -O $ROOT_DIR/Data/test.csv
