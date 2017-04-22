b5cafcbf78f21b4c60a52064b89b85a07273ac0f
Eric Guo <euclidean@172-30-48-214.DYNAPOOL.NYU.EDU>
2017-04-22 13:00:01 -0400

    overwrite existing download

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -7,5 +7,5 @@ cp -r ./home/* $HOME
 
 ROOT_DIR=$HOME/GitHub/JQXXXX/kaggle/modelers/eg/quora
 
-wget --directory-prefix=$ROOT_DIR/Data https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv
-wget --directory-prefix=$ROOT_DIR/Data https://www.dropbox.com/s/ze8odkdw1444e92/test.csv
+wget https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
+wget https://www.dropbox.com/s/ze8odkdw1444e92/test.csv -O $ROOT_DIR/Data/test.csv
