685041010801c1f9075e0a1835c8f473ceaf92e0
Eric Guo <euclidean@172-30-48-214.DYNAPOOL.NYU.EDU>
2017-04-22 12:16:41 -0400

    correct definition of root dir

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -1,4 +1,4 @@
-ROOT_DIR="~/GitHub/JQXXXX/kaggle/modelers/eg/quora"
+ROOT_DIR=$HOME/GitHub/JQXXXX/kaggle/modelers/eg/quora
 
 wget --directory-prefix=$ROOT_DIR/Data https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv
 wget --directory-prefix=$ROOT_DIR/Data https://www.dropbox.com/s/ze8odkdw1444e92/test.csv
