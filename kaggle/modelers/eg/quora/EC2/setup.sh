da25f0b746894e1594c790261bbc7b449e783edd
Eric Guo <euclidean@172-30-48-201.DYNAPOOL.NYU.EDU>
2017-04-22 14:37:08 -0400

    do not overwrite existing setup files

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -2,7 +2,7 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"/kaggle/modelers/eg/quora
 
 for f in .bash_profile .gitconfig .vim .vimrc git-completion.bash git-prompt.sh
 do
-	cp $ROOT_DIR/EC2/$f $HOME
+	cp -n $ROOT_DIR/EC2/$f $HOME
 done
 
 wget https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
