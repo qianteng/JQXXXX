959648fd0c4856c084276b4be8314aacc834a163
Eric Guo <euclidean@172-30-48-201.DYNAPOOL.NYU.EDU>
2017-04-22 14:44:43 -0400

    setup vim molokai

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -1,8 +1,13 @@
+git clone https://github.com/tomasr/molokai.git
+rm -rf $HOME/.vim
+mv molokai $HOME/.vim
+rm -rf molokai
+
 ROOT_DIR="$(git rev-parse --show-toplevel)"/kaggle/modelers/eg/quora
 
-for f in .bash_profile .gitconfig .vim .vimrc git-completion.bash git-prompt.sh
+for f in .bash_profile .gitconfig .vimrc git-completion.bash git-prompt.sh
 do
-	cp -n $ROOT_DIR/EC2/$f $HOME
+	cp $ROOT_DIR/EC2/$f $HOME
 done
 
 wget https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
