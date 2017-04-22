326b8c147efc2020f375439ed363b6b7301bc7b3
Eric Guo <euclidean@172-30-48-201.DYNAPOOL.NYU.EDU>
2017-04-22 14:07:18 -0400

    dynamic parsing of root directory

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -5,7 +5,7 @@ rm -rf molokai
 
 cp -r ./home/* $HOME
 
-ROOT_DIR=$HOME/GitHub/JQXXXX/kaggle/modelers/eg/quora
+ROOT_DIR="$(git rev-parse --show-toplevel)"/kaggle/modelers/eg/quora
 
 wget https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
 wget https://www.dropbox.com/s/ze8odkdw1444e92/test.csv -O $ROOT_DIR/Data/test.csv
