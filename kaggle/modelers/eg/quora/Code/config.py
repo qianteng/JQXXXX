ffa5434c384f7640e58015d9fa3bd979ced12b52
Eric Guo <euclidean@Erics-MBP.home>
2017-04-23 01:17:21 -0400

    start defining config, including root dir

---
+++ kaggle/modelers/eg/quora/Code/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Eric Guo <guoanjie@gmail.com>
+@brief: config for Quora project
+"""
+
+import subprocess
+
+
+# ---------------------- Overall -----------------------
+TASK = 'all'
+# # for testing data processing and feature generation
+# TASK = "sample"
+SAMPLE_SIZE = 1000
+
+# ------------------------ PATH ------------------------
+ROOT_DIR = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).rstrip() + '/kaggle/modelers/eg/quora'
