0f8b5f6a05241bb5ba45437fe6892e8fa5abeef4
Eric Guo <euclidean@172-30-48-84.DYNAPOOL.NYU.EDU>
2017-04-23 14:09:29 -0400

    start data processing script

---
+++ kaggle/modelers/eg/quora/Code/data_process.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Eric Guo <guoanjie@gmail.com>
+@author: Chenglong Chen <c.chenglong@gmail.com>
+@brief: process data
+"""
+
+from utils import time_utils
+
+
+#-------------------------- Main --------------------------
+now = time_utils._timestamp()
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+    main()
