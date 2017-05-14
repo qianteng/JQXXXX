013c0ed3919e18b4b10f8c378e093b26d5e50b3c
Eric Guo <euclidean@Erics-MBP.home>
2017-05-14 05:20:31 -0400

    update setup script

--- kaggle/modelers/eg/quora/EC2/setup.sh
+++ kaggle/modelers/eg/quora/EC2/setup.sh
@@ -4,27 +4,30 @@ mv molokai $HOME/.vim
 rm -rf molokai
 
 ROOT_DIR="$(git rev-parse --show-toplevel)"/kaggle/modelers/eg/quora
+cp $ROOT_DIR/EC2/.vimrc $HOME
 
-for f in $(ls -A)
-do
-	if [ $f != "$(basename "$0")" ]
-		then
-			cp -n $ROOT_DIR/EC2/$f $HOME
-	fi
-done
+# for f in $(ls -A)
+# do
+# 	if [ $f != "$(basename "$0")" ]
+# 		then
+# 			cp -n $ROOT_DIR/EC2/$f $HOME
+# 	fi
+# done
 
-CONDA_DIR=$HOME/anaconda2/bin
-# $CONDA_DIR/conda install -y numpy
+conda install -y numpy
 pip install regex
 
 mkdir -p $ROOT_DIR/Data
-wget -nc https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv -O $ROOT_DIR/Data/train.csv
-wget -nc https://www.dropbox.com/s/ze8odkdw1444e92/test.csv -O $ROOT_DIR/Data/test.csv
+cd $ROOT_DIR/Data
+wget -nc https://www.dropbox.com/s/cpvtr4qpunpkcac/train.csv
+wget -nc https://www.dropbox.com/s/ze8odkdw1444e92/test.csv
 
 mkdir -p $ROOT_DIR/Data/word2vec
-wget -nc https://www.dropbox.com/s/k9fp6t75fdmbt3q/GoogleNews-vectors-negative300.bin.gz -O $ROOT_DIR/Data/word2vec/GoogleNews-vectors-negative300.bin.gz
-gunzip -c $ROOT_DIR/Data/word2vec/GoogleNews-vectors-negative300.bin.gz > $ROOT_DIR/Data/word2vec/GoogleNews-vectors-negative300.bin
+cd $ROOT_DIR/Data/word2vec
+wget -nc https://www.dropbox.com/s/k9fp6t75fdmbt3q/GoogleNews-vectors-negative300.bin.gz
+gunzip GoogleNews-vectors-negative300.bin.gz
 
 mkdir -p $ROOT_DIR/Data/glove/gensim
-wget -nc http://nlp.stanford.edu/data/glove.6B.zip -O $ROOT_DIR/Data/glove/gensim/glove.6B.zip
-unzip $ROOT_DIR/Data/glove/gensim/glove.6B.zip -d $ROOT_DIR/Data/glove/gensim
+cd $ROOT_DIR/Data/glove/gensim
+wget -nc http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
