d44cf172c72000f70169acc88cd43c66035cee31
Eric Guo <euclidean@Erics-MBP.home>
2017-05-14 11:26:38 -0400

    automaticall power off EC2 when done

--- kaggle/modelers/eg/quora/Code/run_data.sh
+++ kaggle/modelers/eg/quora/Code/run_data.sh
@@ -4,6 +4,8 @@
 # @brief: generate all the data and features in one shot
 # @note: if you don't have access to multi-core computers, drop the "&" in the cmd
 
+pids=()
+
 #-----------------------------------------------------------------------
 # prepare data
 python data_preparer.py
@@ -16,43 +18,61 @@ python data_processor.py
 #-----------------------------------------------------------------------
 # generate basic features
 python feature_basic.py &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate distance features
 python feature_distance.py jaccard &
-
+pids+=($!)
 python feature_distance.py edit &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate first and last ngram features
 python feature_first_last_ngram.py &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate intersect features
 python feature_intersect_count.py &
-
+pids+=($!)
 python feature_intersect_position.py &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate match features
 python feature_match.py &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate statistical cooccurrence (weighted) features
 python feature_stat_cooc_tfidf.py tf &
-
+pids+=($!)
 python feature_stat_cooc_tfidf.py tfidf &
-
+pids+=($!)
 python feature_stat_cooc_tfidf.py bm25 &
+pids+=($!)
 
 
 #-----------------------------------------------------------------------
 # generate word2vec features using pre-trained word2vec model
 python feature_word2vec.py google &
-
+pids+=($!)
 python feature_word2vec.py wikipedia &
+pids+=($!)
+
+
+for pid in ${pids[*]}
+do
+	wait $pid
+done
+
+if [ $USER == "ubuntu" ]
+	then
+		sudo poweroff
+fi
