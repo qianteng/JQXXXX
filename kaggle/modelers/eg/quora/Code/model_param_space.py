ebcfd0c557211178ad2338aa56112010c8be13e1
Eric Guo <euclidean@Erics-MBP.home>
2017-05-27 09:39:18 -0400

    don't use eval_metric for now

--- kaggle/modelers/eg/quora/Code/model_param_space.py
+++ kaggle/modelers/eg/quora/Code/model_param_space.py
@@ -58,8 +58,6 @@ param_space_reg_xgb_tree = {
     "colsample_bylevel": hp.quniform("colsample_bylevel", 0.1, 1, 0.05),
     "nthread": xgb_nthread,
     "seed": xgb_random_seed,
-
-    "eval_metric": "logloss",
 }
 
 ## regression with tree booster (parm for best single model)
