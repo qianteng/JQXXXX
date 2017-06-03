cc2f8b9d8d4f865b21d0e1cc22aa5c021e38e892
Eric Guo <euclidean@172-30-48-101.DYNAPOOL.NYU.EDU>
2017-06-03 13:58:22 -0400

    missed 1 function

--- kaggle/modelers/eg/quora/Code/utils/xgb_utils.py
+++ kaggle/modelers/eg/quora/Code/utils/xgb_utils.py
@@ -163,8 +163,8 @@ class XGBClassifier:
                     self.param["subsample"],
                 ))
 
-    def fit(self, X, y, feature_names=None, weight=None):
-        data = xgb.DMatrix(X, label=y, missing=self.missing, feature_names=feature_names, weight=weight)
+    def fit(self, X, y, feature_names=None, sample_weight=None):
+        data = xgb.DMatrix(X, label=y, missing=self.missing, feature_names=feature_names, weight=sample_weight)
         data.set_base_margin(self.base_score*np.ones(X.shape[0] * self.num_class))
         self.model = xgb.train(self.param, data, self.n_estimators)
         return self
